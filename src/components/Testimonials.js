7674d61b15f1006793f6abd8ae3fe68670b36e6a
mikholi <keranemi@icloud.com>
2024-11-05 14:35:30 +0200

    testimonials started

---
+++ src/components/Testimonials.js
@@ -0,0 +1,11 @@
+import customer1image from '../images/customer1.jpg'
+import customer2image from '../images/customer2.jpg'
+import customer3image from '../images/customer3.jpg'
+import customer4image from '../images/customer4.jpg'
+import './testimonials.css'
+import TestimonialCards from './TestimonialCards'
+
+
+const customers = [
+    
+]
